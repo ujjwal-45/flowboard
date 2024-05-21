2d229f86fe23217e8e1242ba610accc19f6c7e19
Ujjwal <ujjwalchoudhary065@gmail.com>
2024-05-21 23:17:44 +0530

    options state management

---
+++ common/recoil/options/index.ts
@@ -0,0 +1,6 @@
+import { useOptions } from "./options.hooks";
+import { optionsAtom } from "./options.atom";
+
+export default optionsAtom;
+
+export { useOptions };
---
+++ common/recoil/options/options.hooks.ts
@@ -0,0 +1,8 @@
+import { useRecoilValue } from 'recoil'
+import { optionsAtom } from './options.atom'
+
+export const useOptions = () => {
+    const options = useRecoilValue(optionsAtom);
+
+    return options;
+}

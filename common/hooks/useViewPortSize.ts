7ba6b073ad57ccaa0405c56e48627555990c612f
Ujjwal <ujjwalchoudhary065@gmail.com>
2024-05-21 23:18:35 +0530

    helper functions

---
+++ common/hooks/useViewPortSize.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react'
+
+export const useViewPortSize = () => {
+    const [height, setHeight] = useState(0);
+    const [width, setWidth] = useState(0);
+    
+    useEffect(() => {
+    const handleResize = () => {
+        setHeight(window.innerHeight);
+        setWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  })
+}

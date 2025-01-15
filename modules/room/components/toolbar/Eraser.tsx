c546f9c52e0a3911e158d9a2e0a652d369cc4bf3
Ujjwal <ujjwalchoudhary065@gmail.com>
2025-01-15 23:12:48 +0530

    erase component

--- modules/room/components/toolbar/Eraser.tsx
+++ modules/room/components/toolbar/Eraser.tsx
@@ -6,7 +6,7 @@ const Eraser = () => {
 
     return (
         <button
-            className={`text-xl ${options.erase && "bg-emerald-400"}`}
+            className={`text-xl text-black ${options.erase && "text-emerald-400"}`}
             onClick={() => {
                 setOptions((prev) => ({ ...prev, erase: !prev.erase }))
             }}

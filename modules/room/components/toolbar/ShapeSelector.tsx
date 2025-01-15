5d9bc73ed862261c50fc87d17213ef80bb3a7d0e
Ujjwal <ujjwalchoudhary065@gmail.com>
2025-01-15 23:15:57 +0530

    shape selector component

---
+++ modules/room/components/toolbar/ShapeSelector.tsx
@@ -0,0 +1,67 @@
+import { useOptions } from "@/common/recoil/options";
+import { AnimatePresence, motion } from "framer-motion";
+import { useRef, useState } from "react"
+import { FaCircle, FaLine, FaSquare } from "react-icons/fa";
+import { LiaPaintBrushSolid } from "react-icons/lia";
+import { useClickAway } from "react-use";
+import { ColorPickerAnimation } from "../../animations/ColorPicker.animations";
+
+const ShapeSelector = () => {
+    const ref = useRef<HTMLDivElement>(null);
+    const [options, setOptions] = useOptions();
+    const [opened, setOpened] = useState(false);
+
+    useClickAway(ref, () => setOpened(false));
+
+    const handleShapeChange = (shape: Shape) => {
+        setOptions((prev) => ({
+            ...prev, shape,
+        }));
+
+        setOpened(false);
+    };
+
+
+    return (
+      <div className="relative flex items-center" ref={ref}>
+        <button className="text-xl" onClick={() => setOpened((prev) => !prev)}>
+          {options.shape === "circle" && <FaCircle />}
+          {options.shape === "line" && <FaLine />}
+          {options.shape === "rect" && <FaSquare />}
+        </button>
+
+        <AnimatePresence>
+          {opened && (
+            <motion.div
+              className="absolute flex left-14 gap-1 rounded-lg bg-zinc-900 p-2"
+              variants={ColorPickerAnimation}
+              initial="from"
+              animate="to"
+              exit="from"
+            >
+              <button
+                className="text-xl"
+                onClick={() => handleShapeChange("circle")}
+              >
+                <FaCircle />
+              </button>
+              <button
+                className="text-xl"
+                onClick={() => handleShapeChange("line")}
+              >
+                <LiaPaintBrushSolid />
+              </button>
+              <button
+                className="text-xl"
+                onClick={() => handleShapeChange("rect")}
+              >
+                <FaSquare />
+              </button>
+            </motion.div>
+          )}
+        </AnimatePresence>
+      </div>
+    );
+}
+
+export default ShapeSelector;

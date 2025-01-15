cae52be726573bbeff69078c18d942d20cf2dd9a
Ujjwal <ujjwalchoudhary065@gmail.com>
2025-01-15 23:12:27 +0530

    added chat

--- modules/room/components/Room.tsx
+++ modules/room/components/Room.tsx
@@ -1,28 +1,31 @@
 import { useRoom } from "@/common/recoil/room";
 import RoomContextProvider from "../context/Room.context";
-import Canvas from "./Canvas";
-import { MousePosition } from "./MousePosition";
-import { MouseRenderer } from "./MouseRenderer";
+import Canvas from "./board/Canvas";
+import { MousePosition } from "./board/MousePosition";
+import { MouseRenderer } from "./board/MouseRenderer";
 import { ToolBar } from "./toolbar/ToolBar";
 import NameInput from "./NameInput";
 import UsersList from "./UsersList";
+import Chat from "./chat/Chat";
+import MoveImage from "./board/MoveImage";
 
 const Room = () => {
   const room = useRoom();
 
   if (!room.id) {
-    return <NameInput />
+    return <NameInput />;
   }
-  
-  
+
   return (
     <RoomContextProvider>
       <div className="absolute h-full w-full overflow-hidden">
         <UsersList />
         <ToolBar />
-        <Canvas />
+        <MoveImage />
+        <Canvas  />
         <MousePosition />
         <MouseRenderer />
+        <Chat />
       </div>
     </RoomContextProvider>
   );

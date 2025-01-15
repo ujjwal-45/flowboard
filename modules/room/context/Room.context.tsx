e8b7cf844626e12b1d2b93f7cbe75f3d2353abe0
Ujjwal <ujjwalchoudhary065@gmail.com>
2025-01-15 23:16:26 +0530

    updated context for multiple refs

--- modules/room/context/Room.context.tsx
+++ modules/room/context/Room.context.tsx
@@ -1,63 +1,106 @@
+import { useState } from "react";
 import { COLORS_ARRAY } from "@/common/constants/colors";
 import { socket } from "@/common/lib/socket";
-import { useSetRoom, useSetUsers } from "@/common/recoil/room/room.hooks";
+import {
+  useRoom,
+  useSetRoom,
+  useSetUsers,
+} from "@/common/recoil/room/room.hooks";
 import { MotionValue, useMotionValue } from "framer-motion";
-import { ReactChild, createContext, useEffect } from "react";
+import { ReactChild, RefObject, createContext, useEffect, useRef } from "react";
+import { toast } from "react-toastify";
 
-export const roomContext = createContext<{ x: MotionValue, y: MotionValue }>(null!);
+export const roomContext = createContext<{
+  x: MotionValue;
+  y: MotionValue;
+  undoRef: RefObject<HTMLButtonElement>;
+  canvasRef: RefObject<HTMLCanvasElement>;
+  bgRef: RefObject<HTMLCanvasElement>;
+  minimapRef: RefObject<HTMLCanvasElement>;
+  moveImage: string;
+  setMoveImage: (base64: string) => void;
+}>(null!);
 
 const RoomContextProvider = ({ children }: { children: ReactChild }) => {
-    const setRoom = useSetRoom();
-    const{handleAddUser,handleRemoveUser} = useSetUsers();
-     const x = useMotionValue(0);
-    const y = useMotionValue(0);
+  const setRoom = useSetRoom();
+  const { users } = useRoom();
+  const { handleAddUser, handleRemoveUser } = useSetUsers();
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
 
-    useEffect(() => {
+  const undoRef = useRef<HTMLButtonElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const bgRef = useRef<HTMLCanvasElement>(null);
+  const minimapRef = useRef<HTMLCanvasElement>(null);
 
-        socket.on("room", (room, usersMovesToParse, usersToParse) => {
-            const usersMoves = new Map<string, Move[]>(JSON.parse(usersMovesToParse));
-            const usersParsed = new Map<string, string>(JSON.parse(usersToParse));
+  const [moveImage, setMoveImage] = useState("");
 
-            const users = new Map<string, User>();
+  useEffect(() => {
+    socket.on("room", (room, usersMovesToParse, usersToParse) => {
+      const usersMoves = new Map<string, Move[]>(JSON.parse(usersMovesToParse));
+      const usersParsed = new Map<string, string>(JSON.parse(usersToParse));
 
-            usersParsed.forEach((name, id) => {
-                if (id === socket.id) return;
+      const newUsers = new Map<string, User>();
 
-                const index = [...usersParsed.keys()].indexOf(id);
+      usersParsed.forEach((name, id) => {
+        if (id === socket.id) return;
 
-                const color = COLORS_ARRAY[index % COLORS_ARRAY.length];
+        const index = [...usersParsed.keys()].indexOf(id);
 
-                users.set(id, {
-                    name,color
-                })
-            })
+        const color = COLORS_ARRAY[index % COLORS_ARRAY.length];
 
-            setRoom((prev) => ({
-                ...prev,
-                users,
-                usersMoves,
-                movesWithoutUser: room.drawed
-            }));
+        newUsers.set(id, {
+          name,
+          color,
         });
+      });
 
-        socket.on("new_user", (userId,username) => {
-            handleAddUser(userId,username);
-        })
-        socket.on("user_disconnected", (userId) => {
-            handleRemoveUser(userId);
+      setRoom((prev) => ({
+        ...prev,
+        users: newUsers,
+        usersMoves,
+        movesWithoutUser: room.drawed,
+      }));
+    });
+
+    socket.on("new_user", (userId, username) => {
+      toast(`${username} joined the room`, {
+        position: "top-right",
+        theme: "colored",
+      });
+      handleAddUser(userId, username);
+    });
+      socket.on("user_disconnected", (userId) => {
+        toast(`${users.get(userId)?.name || "Anonymous"} exited the room`, {
+          position: "bottom-right",
+          theme: "colored",
         });
+      handleRemoveUser(userId);
+    });
 
+    return () => {
+      socket.off("room");
+      socket.off("new_user");
+      socket.off("user_disconnected");
+    };
+  }, [handleAddUser, handleRemoveUser, setRoom, users]);
 
-        return () => {
-            socket.off("room");
-            socket.off("new_user");
-            socket.off("user_disconnected");
-        };
-    }, [handleAddUser,handleRemoveUser,setRoom]);
-
-    return (
-        <roomContext.Provider value={{ x, y }}>{children}</roomContext.Provider>
-    );
+  return (
+    <roomContext.Provider
+      value={{
+        x,
+        y,
+        undoRef,
+        canvasRef,
+        bgRef,
+        minimapRef,
+        moveImage,
+        setMoveImage,
+      }}
+    >
+      {children}
+    </roomContext.Provider>
+  );
 };
 
 export default RoomContextProvider;

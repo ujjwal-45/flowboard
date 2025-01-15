7e7439050da3428c91d1b23137376a3a405f20c8
Ujjwal <ujjwalchoudhary065@gmail.com>
2025-01-15 23:14:52 +0530

    updated toolbar for additional components

--- modules/room/components/toolbar/ToolBar.tsx
+++ modules/room/components/toolbar/ToolBar.tsx
@@ -1,28 +1,64 @@
+import { RefObject } from "react";
+import { useRefs } from "../../hooks/UseRefs";
 import ColorPicker from "./ColorPicker";
 import Eraser from "./Eraser";
 import LineWidthPicker from "./LineWidthPicker";
 import { IoMdColorFill } from "react-icons/io";
+import { FaUndoAlt } from "react-icons/fa";
+import ShapeSelector from "./ShapeSelector";
+import { CANVAS_SIZE } from "@/common/constants/canvasSize";
+import { BsDownload } from "react-icons/bs";
+import ImageChooser from "./ImageChooser";
 
 export const ToolBar = () => {
+
+  const { canvasRef, undoRef, bgRef } = useRefs();
+
+  const handleDownload = () => {
+    const canvas = document.createElement("canvas");
+    canvas.height = CANVAS_SIZE.height
+    canvas.width = CANVAS_SIZE.width
+
+    const tempCtx = canvas.getContext("2d")
+
+    if (tempCtx && canvasRef.current && bgRef.current) {
+      tempCtx.drawImage(bgRef.current, 0, 0);
+      tempCtx.drawImage(canvasRef.current, 0, 0)
+    }
+
+    const link = document.createElement("a");
+    link.href = canvas.toDataURL("image/png")
+    link.download = "canvas.png"
+    link.click()
+  }
+
   return (
     <div
-      className="absolute flex flex-col top-[50%] left-10 z-50 p-2 gap-5 items-center rounded-lg bg-slate-900 text-neutral-200"
+      className="absolute flex flex-col top-[50%] left-10 z-50 p-2 gap-5 items-center rounded-lg bg-white border-2 border-black text-neutral-200"
       style={{ transform: "translateY(-50%)" }}
     >
+      <button className="text-xl text-black" ref={undoRef}>
+        <FaUndoAlt />
+      </button>
       <ColorPicker />
       <LineWidthPicker />
-      <Eraser/>
-      <button className="text-xl">
+      <ShapeSelector />
+      <Eraser />
+      <ImageChooser />
+      <button className="text-xl text-black">
         <IoMdColorFill />
       </button>
-      <button className="text-xl">
+      <button className="text-xl text-black">
         <IoMdColorFill />
       </button>
-      <button className="text-xl">
+      <button className="text-xl text-black">
         <IoMdColorFill />
       </button>
-      <button className="text-xl">
+      <button className="text-xl text-black">
         <IoMdColorFill />
+      </button>
+      <button className="text-xl text-black" onClick={handleDownload}>
+        <BsDownload />
       </button>
     </div>
   );
